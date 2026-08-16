b532c7ee05b3ce1c6088d5f59e9a92b547a41455
raingoh09-code <raingoh09@gmail.com>
2026-08-16 15:33:19 +0800

    folder added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#with the help of gitbash 
+#creating directory 
+mkdir -p src
+mkdir -p research 
+
+#create files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created succesfully" 
